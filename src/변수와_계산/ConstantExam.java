fd1cd6ed432cb60efa2be426ddaaf937c43ed6ba
hhyukk <hhyukk1273@gmail.com>
2025-10-23 23:22:09 +0900

    상수
    상수란 수식에서 변하지 않는 값을 의미한다.

---
+++ src/변수와_계산/ConstantExam.java
@@ -0,0 +1,18 @@
+package 변수와_계산;
+
+public class ConstantExam {
+    public static void main(String[] args) {
+        final int J;
+        J = 10;
+
+//        J = 5;
+
+        double circleArea;
+        final double PI = 3.14159;
+        circleArea = 3 * 3 * PI;
+
+        final int OIL_PRICE = 1490;
+
+        int totalPrice = 50 * OIL_PRICE;
+    }
+}
