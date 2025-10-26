805d21e78f833ede0dbf9794421d7d846741d7e0
hhyukk <hhyukk1273@gmail.com>
2025-10-26 15:34:18 +0900

    연산자 우선순위

---
+++ src/변수와_계산/Operator3.java
@@ -0,0 +1,14 @@
+package 변수와_계산;
+
+public class Operator3 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = 15;
+
+        System.out.println((a - b) * c);
+        System.out.println(a > 5 || b > 5);
+        System.out.println(a++ - 5);
+        System.out.println(a);
+    }
+}
