71a3827bd61ef8e401ecfe6fbde14bb06b76e8a8
hhyukk <hhyukk1273@gmail.com>
2025-10-26 11:19:10 +0900

    기본형 타입
    기본형 타입은 가장 기본이 되는 데이터 타입으로써 정수형, 실수형, 문자형, 불린형을 의미한다.

---
+++ src/변수와_계산/PrimitiveDataType.java
@@ -0,0 +1,18 @@
+package 변수와_계산;
+
+public class PrimitiveDataType {
+    public static void main(String[] args) {
+        boolean isFun = false;
+        System.out.println(isFun);
+
+        char c = 'f';
+
+        int x = 59;
+
+        long bing = 343434333L;
+
+        float f = 32.4f;
+
+        double d = 34343434.5;
+    }
+}
