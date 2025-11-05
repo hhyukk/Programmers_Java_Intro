73b51fcdccdd6229cfa25d17341f1e9ef3d4f2cc
hhyukk <hhyukk1273@gmail.com>
2025-11-05 23:24:23 +0900

    인터페이스 만들기
    인터페이스: 서로 관계가 없는 물체들이 상호 작용을 하기 위해서 사용하는 장치나 시스템

---
+++ src/인터페이스/TV.java
@@ -0,0 +1,14 @@
+package 인터페이스;
+
+public interface TV {
+    public int MIN_VOLUME = 0;
+    public int MAX_VOLUME = 100;
+
+    public void turnOn();
+
+    public void turnOff();
+
+    public void changeVolume(int volume);
+
+    public void changeChannel(int channel);
+}
