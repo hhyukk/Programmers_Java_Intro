683a4d3b0a09a601ef8bcb1c6e3e4c3eccd336df
hhyukk <hhyukk1273@gmail.com>
2025-11-06 00:00:01 +0900

    내부클래스
    내부 클래스란 클래스 안에 선언된 클래스 (인스턴스 클래스, static 클래스, 지역 클래스, 익명 클래스)

---
+++ src/인터페이스/InnerExam1.java
@@ -0,0 +1,18 @@
+package 인터페이스;
+
+public class InnerExam1 {
+    class Cal {
+        int value = 0;
+
+        public void plus() {
+            value++;
+        }
+    }
+
+    public static void main(String[] args) {
+        InnerExam1 t = new InnerExam1();
+        InnerExam1.Cal cal = t.new Cal();
+        cal.plus();
+        System.out.println(cal.value);
+    }
+}
