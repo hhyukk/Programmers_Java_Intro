492b0f99c116ef067eb3c13274dc038a238eee63
hhyukk <hhyukk1273@gmail.com>
2025-11-06 00:04:50 +0900

    익명클래스
    익명 중첩 클래스는 익명 클래스라고 보통 말하며, 내부 클래스이기도 하다.

---
+++ src/인터페이스/ActionExam.java
@@ -0,0 +1,16 @@
+package 인터페이스;
+
+public class ActionExam {
+    public static void main(String[] args) {
+//        Action action=new MyAction();
+//        action.exec();
+
+        Action action = new Action() {
+            @Override
+            public void exec() {
+                System.out.println("exec");
+            }
+        };
+        action.exec();
+    }
+}
---
+++ src/인터페이스/MyAction.java
@@ -0,0 +1,9 @@
+package 인터페이스;
+
+public class MyAction extends Action {
+
+    @Override
+    public void exec() {
+        System.out.println("exec");
+    }
+}
